1974b17fded28eaaab216fe8c00b2929adb6a896
Tanner Hodges <austintannerhodges@gmail.com>
2017-02-02 17:02:15 -0500

    Replace threshold with raw buffer manipulation
    This solve isn't perfect, but it's better than it was.

--- index.js
+++ index.js
@@ -11,17 +11,16 @@ const VALID_EXTS = ['.png'];
 
 /**
  * Get SVG content.
- * @param  {Vinyl}   file
- * @param  {Buffer}  output
- * @param  {object}  info
+ * @param  {Buffer}  buffer
+ * @param  {Object}  params  {cwd, base, path, width, height}
  * @return {Buffer}
  */
-function getSvg(file, output, info) {
+function getSvg(buffer, params) {
   let newFile = new gutil.File({
-    cwd: file.cwd,
-    base: file.base,
-    path: file.path,
-    contents: new Buffer(`<svg width="${info.width}" height="${info.height/2}" viewBox="0 0 ${info.width} ${info.height/2}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    cwd: params.cwd,
+    base: params.base,
+    path: params.path,
+    contents: new Buffer(`<svg width="${params.width}" height="${params.height/2}" viewBox="0 0 ${params.width} ${params.height/2}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <filter id="zorrosvg" primitiveUnits="objectBoundingBox">
       <feOffset in="SourceGraphic" result="bottom-half" dy="-0.5"></feOffset>
@@ -29,7 +28,7 @@ function getSvg(file, output, info) {
       <feComposite in="SourceGraphic" in2="alpha-mask" operator="in"></feComposite>
     </filter>
   </defs>
-  <image width="100%" height="200%" filter="url(#zorrosvg)" xlink:href="data:image/jpeg;base64,${output.toString('base64')}"></image>
+  <image width="100%" height="200%" filter="url(#zorrosvg)" xlink:href="data:image/jpeg;base64,${buffer.toString('base64')}"></image>
 </svg>`)
   });
 
@@ -45,7 +44,7 @@ function getSvg(file, output, info) {
  * https://github.com/sindresorhus/gulp-imagemin/blob/master/index.js
  * https://github.com/mahnunchik/gulp-responsive/blob/master/lib/index.js
  *
- * @param  {object}  options
+ * @param  {Object}  options
  */
 module.exports = (options) => {
   options = Object.assign({
@@ -78,28 +77,59 @@ module.exports = (options) => {
      * Note: Assumes image has an alpha channel.
      */
     let image = sharp(file.contents);
+    let width, height, channels;
     let errorPrefix = `${chalk.red('✘')} File \`${file.relative}\`: `;
 
-    // Create a white-out copy of the image
-    image.clone().threshold(1).toBuffer((error, output, info) => {
+    // Get image metadata so we can convert buffer from `raw()` back to a sharp instance
+    // https://github.com/lovell/sharp/issues/552#issuecomment-243452196
+    image.metadata(function(error, metadata) {
+      width = metadata.width;
+      height = metadata.height;
+      channels = metadata.channels;
+    // Get a copy of the image's raw pixel data
+    }).clone().raw().toBuffer((error, buffer, info) => {
       if (error) {
         error.message = errorPrefix + error.message;
         return callback(new gutil.PluginError(PLUGIN_NAME, error, { showStack: true }));
       }
 
-      // Resize the canvas to fit both images
-      return image.extend({ top: 0, right: 0, bottom: info.height, left: 0 })
-        // Paste the white-out copy
-        .overlayWith(output, { top: info.height, left: 0 })
-        // Save as JPG
-        .jpeg()
-        .toBuffer(function(error, output, info) {
+      // Create a luminance mask based on the image's alpha channel
+      for (var i = 0; i < buffer.length; i += 4) {
+        buffer[i + 0] = buffer[i + 3];
+        buffer[i + 1] = buffer[i + 3];
+        buffer[i + 2] = buffer[i + 3];
+        buffer[i + 3] = 255;
+      }
+
+      // Convert buffer back to a sharp instance
+      sharp(buffer, { raw: { width: width, height: height, channels: channels } })
+        .png().toBuffer(function(error, buffer, info) {
+        if (error) {
+          error.message = errorPrefix + error.message;
+          return callback(new gutil.PluginError(PLUGIN_NAME, error, { showStack: true }));
+        }
+
+        // Resize the canvas to fit both images
+        return image
+          .background({ r: 255, g: 255, b: 255, alpha: 1 })
+          .extend({ top: 0, right: 0, bottom: info.height, left: 0 })
+          // Paste the luminance mask at the bottom
+          .overlayWith(buffer, { top: info.height, left: 0 })
+          // Compress as JPG
+          .jpeg().toBuffer(function(error, buffer, info) {
           if (error) {
             error.message = errorPrefix + error.message;
             return callback(new gutil.PluginError(PLUGIN_NAME, error, { showStack: true }));
           }
 
-          let newFile = getSvg(file, output, info);
+          // Final SVG conversion
+          let newFile = getSvg(buffer, {
+            cwd: file.cwd,
+            base: file.base,
+            path: file.path,
+            width: info.width,
+            height: info.height
+          });
 
           totalFiles++;
 
@@ -110,6 +140,7 @@ module.exports = (options) => {
           // TODO: Responsive sizes
           callback(null, newFile);
         });
+      });
     });
   }, callback => {
     gutil.log(`${PLUGIN_NAME}: Generated ${totalFiles} alpha ${plur('mask', totalFiles)}.`);
