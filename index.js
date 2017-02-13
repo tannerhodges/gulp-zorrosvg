9b9dd68feb324ea06447686a2b8d5a94e65f4e9a
Tanner Hodges <austintannerhodges@gmail.com>
2017-02-13 15:59:09 -0500

    Progress: Simplify original mask logic
    Final output still isn't perfect. Need to get gamma correction working for top half of the composite.

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function getSvg(buffer, params) {
     cwd: params.cwd,
     base: params.base,
     path: params.path,
-    contents: new Buffer(`<svg width="${params.width}" height="${params.height/2}" viewBox="0 0 ${params.width} ${params.height/2}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    contents: Buffer.from(`<svg width="${params.width}" height="${params.height / 2}" viewBox="0 0 ${params.width} ${params.height / 2}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <filter id="zorrosvg" primitiveUnits="objectBoundingBox">
       <feOffset in="SourceGraphic" result="bottom-half" dy="-0.5"></feOffset>
-      <feColorMatrix type="matrix" in="bottom-half" result="alpha-mask" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 0"></feColorMatrix>
-      <feComposite in="SourceGraphic" in2="alpha-mask" operator="in"></feComposite>
+      <feColorMatrix type="matrix" in="bottom-half" result="luma-mask" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 0"></feColorMatrix>
+      <feComposite in="SourceGraphic" in2="luma-mask" operator="in"></feComposite>
     </filter>
   </defs>
   <image width="100%" height="200%" filter="url(#zorrosvg)" xlink:href="data:image/jpeg;base64,${buffer.toString('base64')}"></image>
@@ -38,7 +38,7 @@ function getSvg(buffer, params) {
 }
 
 /**
- * Generate ZorroSVG alpha masks from PNGs.
+ * Generate ZorroSVG luminance masks from PNGs.
  *
  * Based on the following plugins:
  * https://github.com/sindresorhus/gulp-imagemin/blob/master/index.js
@@ -77,134 +77,90 @@ module.exports = (options) => {
      * Note: Assumes image has an alpha channel.
      */
     let image = sharp(file.contents);
-    let width, height, channels;
     let errorPrefix = `${chalk.red('✘')} File \`${file.relative}\`: `;
 
-    // Get image metadata so we can convert buffer from `raw()` back to a sharp instance
-    // https://github.com/lovell/sharp/issues/552#issuecomment-243452196
-    image.metadata(function(error, metadata) {
-      width = metadata.width;
-      height = metadata.height;
-      channels = metadata.channels;
-    // Get a copy of the image's raw pixel data
-    }).clone().raw().toBuffer((error, buffer, info) => {
+    // Create a canvas 2x the height of the original image.
+    // Keep the original image on top and paste a copy of it on the bottom.
+    image.toBuffer(function(error, imageBuffer, imageInfo) {
       if (error) {
         error.message = errorPrefix + error.message;
         return callback(new gutil.PluginError(PLUGIN_NAME, error, { showStack: true }));
       }
-
-      // Create a luminance mask based on the image's alpha channel
-      // for (var i = 0; i < buffer.length; i += 4) {
-      //   buffer[i + 0] = buffer[i + 3];
-      //   buffer[i + 1] = buffer[i + 3];
-      //   buffer[i + 2] = buffer[i + 3];
-      //   buffer[i + 3] = 255;
-      // }
-
-
-      // ------------------------------
-      // BEGIN ZORROSVG
-      // ------------------------------
-
-      // Based on Quasimondo's `createMaskedMap` function
-      // @see https://github.com/Quasimondo/QuasimondoJS/blob/ce7ffb317f7435940046d5ff46a7503f92efd328/zorrosvg/js/zorrosvgmaskmaker.js#L176-L277
-      let isLittleEndian;
-      let applyGammaCorrection = true;
-
-      try {
-        if (isLittleEndian == undefined) {
-          // Determine whether Uint32 is little- or big-endian.
-          var endianTest = new ArrayBuffer(4);
-          var e8 = new Uint8ClampedArray(endianTest);
-          var e32 = new Uint32Array(endianTest);
-          e32[0] = 0x000000ff;
-          isLittleEndian = (e8[3] === 0xff);
-        }
-      } catch (error) {
-        error.message = errorPrefix + error.message;
-        return callback(new gutil.PluginError(PLUGIN_NAME, error, { showStack: true }));
-      }
-      console.log(isLittleEndian ? '✅  Yes, this is a Little Endian machine' : '❌  No, this is not a Little Endian machine');
-
-      var buf32 = new Uint32Array(buffer);
-      var l = buf32.length;
-      var l2 = l>>1;
-
-      if (!isLittleEndian) {
-        if (applyGammaCorrection) {
-          for (var i = l; --i >= l2;) {
-            // Gamma correction for semi transparent pixels:
-            var b = (Math.pow(((buf32[i] >>> 24) & 0xff) / 255,0.45 ) * 255) | 0;
-            buf32[i] = 0xff000000 | ( b << 16) | ( b << 8) | b;
-          }
-        } else {
-          for (var i = l; --i >= l2;) {
-            // No Gamma correction on some platforms
-            var b = (buf32[i] >>> 24) & 0xff;
-            buf32[i] = 0xff000000 | ( b << 16) | ( b << 8) | b;
-          }
-        }
-        for (i = l2; --i > -l;) {
-          buf32[i] |= 0xff000000;
-        }
-      } else {
-        if (applyGammaCorrection) {
-          for (var i = l; --i >= l2;) {
-            // Gamma correction for semi transparent pixels:
-            var b = (Math.pow(( buf32[i] & 0xff) / 255,0.45 ) * 255) | 0;
-            buf32[i] = 0xff | ( b << 24) | ( b << 16) | (b<<8);
-          }
-        } else {
-          for (var i = l; --i >= l2;) {
-            // No Gamma correction on some platforms
-            var b = buf32[i] & 0xff;
-            buf32[i] = 0xff | ( b << 24) | ( b << 16) | (b<<8);
-          }
-        }
-        for (i = l2; --i > -l;) {
-          buf32[i] |= 0xff;
-        }
-      }
-
-      // DEBUG
-      // console.log(buffer, buf32);
-      buffer = Buffer.from(buf32);
-      console.log(buffer);
-
-      // ------------------------------
-      // END ZORROSVG
-      // ------------------------------
-
-
-
-      // Convert buffer back to a sharp instance
-      sharp(buffer, { raw: { width: width, height: height, channels: channels } })
-        .png().toBuffer(function(error, buffer, info) {
+      // Force transparent background
+      return image.background({ r: 0, g: 0, b: 0, alpha: 0 })
+      // Double canvas height
+        .extend({ top: 0, right: 0, bottom: imageInfo.height, left: 0 })
+      // Paste image
+        .overlayWith(imageBuffer, { top: imageInfo.height, left: 0 })
+      // Get raw pixel data to manipulate
+        .raw().toBuffer(function(error, compositeBuffer, compositeInfo) {
         if (error) {
           error.message = errorPrefix + error.message;
           return callback(new gutil.PluginError(PLUGIN_NAME, error, { showStack: true }));
         }
 
-        // Resize the canvas to fit both images
-        return image
-          .extend({ top: 0, right: 0, bottom: info.height, left: 0 })
-          // Paste the luminance mask at the bottom
-          .overlayWith(buffer, { top: info.height, left: 0 })
-          // Compress as JPG
-          .jpeg().toBuffer(function(error, buffer, info) {
+        // TOP HALF
+        for (let i = 0; i < compositeBuffer.length / 2; i = i + 4) {
+          compositeBuffer[i + 0] = (Math.pow(compositeBuffer[i + 0] / 255, 0.45) * 255) | 0;
+          compositeBuffer[i + 1] = (Math.pow(compositeBuffer[i + 1] / 255, 0.45) * 255) | 0;
+          compositeBuffer[i + 2] = (Math.pow(compositeBuffer[i + 2] / 255, 0.45) * 255) | 0;
+          compositeBuffer[i + 3] = 255;
+        }
+        // BOTTOM HALF
+        for (let i = compositeBuffer.length / 2; i < compositeBuffer.length; i = i + 4) {
+          let alpha = compositeBuffer[i + 3];
+          alpha = (Math.pow(alpha / 255, 0.45) * 255) | 0;
+          compositeBuffer[i + 0] = alpha;
+          compositeBuffer[i + 1] = alpha;
+          compositeBuffer[i + 2] = alpha;
+          compositeBuffer[i + 3] = 255;
+        }
+
+// ORIGINAL
+// let buf32 = new Uint32Array(compositeBuffer);
+// let l = buf32.length;
+// let l2 = l >> 1;
+//
+// // Loop through the bottom half of the image
+// // Gamma correction for semi transparent pixels
+// for (let i = l; --i >= l2;) {
+//   let b = (Math.pow(((buf32[i] >>> 24) & 0xff) / 255, 0.45) * 255) | 0;
+//   buf32[i] = 0xff000000 | (b << 16) | (b << 8) | b;
+// }
+// // Loop through the top half of the image
+// // And then some?
+// for (let i = l2; --i > -l;) {
+//   buf32[i] |= 0xff000000;
+// }
+//
+// console.log(buf32);
+// compositeBuffer = Buffer.from(buf32);
+
+        // Compress as JPG
+        sharp(compositeBuffer, { raw: { width: compositeInfo.width, height: compositeInfo.height, channels: compositeInfo.channels } })
+          .jpeg().toBuffer(function(error, finalBuffer, finalInfo) {
           if (error) {
             error.message = errorPrefix + error.message;
             return callback(new gutil.PluginError(PLUGIN_NAME, error, { showStack: true }));
           }
 
           // Final SVG conversion
-          let newFile = getSvg(buffer, {
+          let newFile = getSvg(finalBuffer, {
             cwd: file.cwd,
             base: file.base,
             path: file.path,
-            width: info.width,
-            height: info.height
+            width: finalInfo.width,
+            height: finalInfo.height
           });
+
+          // DEBUG
+          // let newFile = new gutil.File({
+          //   cwd: file.cwd,
+          //   base: file.base,
+          //   path: file.path,
+          //   contents: finalBuffer
+          // });
+          // newFile.basename = newFile.basename.replace('.png', '.jpg');
 
           totalFiles++;
 
@@ -218,7 +174,7 @@ module.exports = (options) => {
       });
     });
   }, callback => {
-    gutil.log(`${PLUGIN_NAME}: Generated ${totalFiles} alpha ${plur('mask', totalFiles)}.`);
+    gutil.log(`${PLUGIN_NAME}: Generated ${totalFiles} ZorroSVG ${plur('file', totalFiles)}.`);
     callback();
   });
 };
